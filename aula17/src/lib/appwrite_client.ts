c5a7a84ed41dd08c5f2f250e0e13f9dddcd337b0
tcguus <gugucram15@gmail.com>
2024-10-07 10:03:52 -0300

    Commit finalizado.

--- aula17/src/lib/appwrite_client.ts
+++ aula17/src/lib/appwrite_client.ts
@@ -5,5 +5,6 @@ const client = new Client();
 client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
+    
 
 export default client;
