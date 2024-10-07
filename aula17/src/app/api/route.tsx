bc344bbcf96b444611ab683f63bcf98082935e84
nathan <nathan.consolo17@gmail.com>
2024-10-07 08:57:52 -0300

    Configurando o cliente do appwrite

--- aula17/src/app/api/route.tsx
+++ aula17/src/app/api/route.tsx
@@ -2,11 +2,11 @@ import { TipoParametro } from "@/types";
 import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
 
-export async function GET(request:Request,{params}:{params:{id:string}}) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
     try {
         // Leitura do arquivo JSON na pasta src/data/base.json
         const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
-        
+
         // Parsing do JSON e retorno da resposta
         const data = JSON.parse(file);
         return NextResponse.json(data);
